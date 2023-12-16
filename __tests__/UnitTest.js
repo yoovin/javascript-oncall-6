b9f297ebb94c4442ff81b54be654a354c4755df0
yoovin <soo22839@gmail.com>
2023-12-16 15:03:11 +0900

    test: 공백 입력 테스트 추가

--- __tests__/UnitTest.js
+++ __tests__/UnitTest.js
@@ -3,6 +3,13 @@ import App from '../src/App.js';
 describe('기능 테스트', () => {
     const app = new App();
 
+    //날짜 
+    test('날짜 입력이 공백인 경우', () => {
+        expect(() => {
+            app.getMonthAndDay('');
+        }).toThrow('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
+    });
+
     test('월이 1~12의 숫자가 아닌 경우', () => { 
         expect(() => {
             app.getMonthAndDay('13, 월');
@@ -27,6 +34,19 @@ describe('기능 테스트', () => {
 
     test('요일이 월~일인 경우', () => {
         expect(app.getMonthAndDay('6, 수')).toEqual([6, '수']);
+    });
+
+    test('날짜 입력이 공백인 경우', () => {
+        expect(() => {
+            app.getMonthAndDay('');
+        }).toThrow('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
+    });
+
+    // 근무자
+    test('근무자 입력이 공백인 경우', () => {
+        expect(() => {
+            app.getWorkers('');
+        }).toThrow('[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.');
     });
 
     test('근무자의 이름이 중복되는 경우', () => {
